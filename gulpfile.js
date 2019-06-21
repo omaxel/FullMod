b106e4874d8eee217d9c28612d01f545246a083d
Omar Muscatello <7016897+OmarMuscatello@users.noreply.github.com>
2019-06-21 09:35:20 +0200

    Updated dependencies
    Updated dependencies to remove vulnerabilities in node modules.
    Added script to build files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var rename = require("gulp-rename");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   return gulp.src('./src/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('minify-js', function () {
+gulp.task('minify-js', () => {
   return gulp.src('./src/**/*.js')
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('default', gulp.parallel('sass', 'minify-js'));
